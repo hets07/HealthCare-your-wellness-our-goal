ebf049a3a9c3a338bcb6fcb5b2ad8f65911525bd
hets07 <145050563+hets07@users.noreply.github.com>
2025-03-02 03:45:31 +0530

    Update vite.config.js

--- NODE/client/vite.config.js
+++ NODE/client/vite.config.js
@@ -3,16 +3,17 @@ import react from '@vitejs/plugin-react'
 import compression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+            compression({
+            algorithm: 'gzip',  
+            ext: '.gz',          
+            deleteOriginFile: false, 
+            threshold: 1024    
+        })],
  server: {
   host: '0.0.0.0',
   port: process.env.PORT || 5173,
   allowedHosts: ['healthcare-your-wellness-our-goal-4.onrender.com']
 },
-  compression({
-            algorithm: 'gzip',  
-            ext: '.gz',          
-            deleteOriginFile: false, 
-            threshold: 1024    
-        })
+ 
 })
