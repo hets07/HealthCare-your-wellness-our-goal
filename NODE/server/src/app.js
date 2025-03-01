eb31e82369076a504ff3de9078e805d4f03ddae1
hets07 <145050563+hets07@users.noreply.github.com>
2025-03-02 05:17:16 +0530

    Update app.js

--- NODE/server/src/app.js
+++ NODE/server/src/app.js
@@ -28,11 +28,10 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
 });
 const transporter = nodemailer.createTransport({
     service: 'gmail',
